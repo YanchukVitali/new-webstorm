ecaea60d3207b5248b650d9c8bc65b6b50e8ab52
Vital <Vitalii8881878@gmail.com>
2023-09-12 21:19:53 -0400

    add component new Star and debugger

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,39 @@ import './App.css';
 import {findAllByDisplayValue} from "@testing-library/react";
 
 // function declaration
+
+function hello(){
+    debugger
+    alert("Hello IT-Kamasutra")
+}
+hello();
+
 function App() {
+    debugger
     return (
         <div>
             This is APP component
+
+            <Accordion />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
         </div>
+
     );
 }
 
+function Star(){
+    debugger
+    return(
 
-function Rating() {
+        <div>Star</div>
+    );
+}
+/*function Rating() {
+    debugger
     return (
         <div>
             <div>Star</div>
@@ -21,13 +44,12 @@ function Rating() {
             <div>Star</div>
             <div>Star</div>
             <div>Star</div>
-
-
         </div>
-    )
+    );
 }
-
+*/
     function Accordion(){
+        debugger
     return (
 <div>
     <h3>Menu</h3>
@@ -37,6 +59,6 @@ function Rating() {
         <li>3</li>
     </ul>
 </div>
-    )
+    );
     }
 export default App;
